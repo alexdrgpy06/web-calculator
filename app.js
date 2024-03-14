d39e04ca1719380c67a2a6e2d6c2c6acea336142
alexdrgpy06 <116830034+alexdrgpy06@users.noreply.github.com>
2024-03-14 11:00:04 -0300

    Update calculator JS for better functionality

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
 var displayValue = "";
-function display(number) {
-    displayValue += number;
+var lastOperator = null;
+var lastValue = null;
+
+function display(value) {
+    if (['~', '/', '*', '-', '+\'].includes(value)) {
+        if (lastOperator && lastValue) {
+            calculate();
+        }
+        lastOperator = value;
+        lastValue = null;
+    } else {
+        if (lastOperator) {
+            if (lastValue == null) {
+                lastValue = displayValue;
+                displayValue = value;
+            } else {
+                displayValue += value;
+            }
+        } else {
+            displayValue += value;
+        }
+    }
     updateDisplay();
 }
+
 function updateDisplay() {
     document.getElementById('display').innerHTML = displayValue;
 }
+
 function clear() {
     displayValue = "";
+    lastOperator = null;
+    lastValue = null;
     updateDisplay();
 }
-function operate(operation) {
-    if (operation === "=") {
-        try {
-            displayValue = eval(displayValue).toString();
-        } catch (error) {
-            console.error(error);
-            di
+
+function calculate() {
+    if (lastOperator && lastValue) {
+        var result;
+        switch (lastOperator) {
+            case '/':
+                result = number(lastValue) / number(displayValue);
+                break;
+            case '*':
+                result = number(lastValue) * number(displayValue);
+                break;
+            case '-':
+                result = number(lastValue) - number(displayValue);
+                break;
+            case '+':
+                result = number(lastValue) + number(displayValue);
+                break;
+        }
+        displayValue = result.toString();
+        lastValue = null;
+        lastOperator = null;
+        updateDisplay();
+    }
+}
